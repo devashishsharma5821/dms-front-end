774176b3e6035701119568e75f4ffa4363efb192
Bampoori <SB6822@zebra.com>
2022-11-18 13:17:19 -0500

    add menu.ts file

--- src/styles/DMSThemeProvider.ts
+++ src/styles/DMSThemeProvider.ts
@@ -1,6 +1,7 @@
 import { extendTheme } from '@chakra-ui/react';
 import { styles, colors, Button, spacing, sizes } from '../models/theme';
 import { Drawer } from '../models/drawer';
+import { Menu } from '../models/menu';
 export const dmsTheme = extendTheme({
     styles,
     colors,
@@ -8,6 +9,7 @@ export const dmsTheme = extendTheme({
     ...sizes,
     components: {
         Button,
-        Drawer
+        Drawer,
+        Menu,
     }
 });
