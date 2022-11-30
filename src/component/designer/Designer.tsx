ad8719cbc774f5adca11888168730bb6fd6d39bd
ZGN\AG7955 <arjun.guntuka@zebra.com>
2022-11-30 01:37:55 -0600

    canvas Ui changes added

--- src/component/designer/Designer.tsx
+++ src/component/designer/Designer.tsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useRef } from 'react';
 import { dia, shapes, ui } from '@antuit/rappid-v1';
 import './designer.scss';
-import { Flex, Wrap, Button } from '@chakra-ui/react';
+import { Flex, Wrap, Button, Box, IconButton } from '@chakra-ui/react';
 import * as appShapes from '../../models/app-shapes';
+import ZoomInIcon from '../../assets/icons/ZoomInIcon';
+import ZoomOutIcon from '../../assets/icons/ZoomOutIcon';
 const Designer = (props: any) => {
     const canvas: any = useRef(null);
+    const designerElement = React.useRef<HTMLDivElement | null>(null);
+    const [ paperState, setPaperState ] = React.useState<dia.Paper | undefined>(undefined);
+    const [ paperScrollerState, setPaperScrollerState ] = React.useState<any>(undefined);
     let paperScroller: any;
     useEffect(() => {
         const graph = new dia.Graph({}, { cellNamespace: shapes });
         const paper = new dia.Paper({
+            width: 0,
+            height: 0,
             model: graph,
             gridSize:           20,
             drawGrid: true,
@@ -42,7 +49,6 @@ const Designer = (props: any) => {
             scrollWhileDragging: true,
             cursor: 'grab'
         });
-
         canvas.current.appendChild(paperScroller.el);
         paperScroller.render().center();
         const nav = new ui.Navigator({
@@ -97,6 +103,8 @@ const Designer = (props: any) => {
         m2.attr(".label/text", "Model 2");
         paper.unfreeze();
         paper.on('blank:pointerdown', paperScroller.startPanning);
+        setPaperScrollerState(paperScroller)
+        setPaperState(paper);
         return () => {
             paperScroller.remove();
             nav.remove();
@@ -105,18 +113,34 @@ const Designer = (props: any) => {
     }, []);
 
     const zoomIn = () => {
-        paperScroller.zoom(0.2, { max: 4 });
+        paperScrollerState.zoom(0.2, { max: 4 });
     };
     const zoomOut = () => {
-        paperScroller.zoom(-0.2, { min: 0.2 });
+        paperScrollerState.zoom(-0.2, { min: 0.2 });
     };
+    React.useEffect( () => {
+        const setPaperDimensions = () =>{
+            let width = designerElement.current?.clientWidth || 20;
+            let height = designerElement.current?.clientHeight || 20; 
+            paperState?.setDimensions(width, height);
+        }
+        setPaperDimensions();
+        window.addEventListener('resize', setPaperDimensions);
+        return () => {window.removeEventListener('resize', setPaperDimensions); }
+        },
+        [paperState]
+    );
     return (
-        <>
-            <Button onClick={zoomIn}>Zoom In</Button>
-            <Button onClick={zoomOut}>Zoom Out</Button>
-            <Wrap className="canvas" ref={canvas}/>
-            <Wrap id="navigator"/>
-        </>
+        <Box position="relative">
+            <Box position="absolute" zIndex={10} bottom="75px" right="180px">
+                {/* <Button onClick={zoomIn} mr="5px" backgroundColor="#808080" _hover={{bg:"#929090"}}>Zoom In</Button>
+                <Button onClick={zoomOut} backgroundColor="#808080" _hover={{bg:"#929090"}}>Zoom Out</Button> */}
+                <IconButton aria-label='Zoom In' variant='outline' icon={<ZoomInIcon />} onClick={zoomIn}/>
+                 <IconButton aria-label='Zoom Out' variant='outline' icon={<ZoomOutIcon />} onClick={zoomOut} />
+            </Box>
+            <Box ref={designerElement} className="designer"><Wrap className="canvas" ref={canvas} /></Box>
+            <Box position="absolute" bottom="75px"><Wrap id="navigator" /></Box>
+        </Box>
 
     );
 };
