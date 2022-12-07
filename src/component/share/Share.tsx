9ddf9499957600563d688446f93cbba14a420425
Bampoori <SB6822@zebra.com>
2022-12-07 16:35:24 -0500

    share modal style and data added

--- src/component/share/Share.tsx
+++ src/component/share/Share.tsx
@@ -12,8 +12,34 @@ const Share = (props: any) => {
     const {  onClose } = useDisclosure()
     const initialRef = React.useRef(null)
     const finalRef = React.useRef(null)
-    return (
 
+const shareData=[
+    { firstName: 'Shirin',
+    lastName: 'Bampoori',
+    email: 'shirin.bampoori@antuit.com',
+    },
+    { firstName: 'Zubin',
+    lastName: 'Shah',
+    email: 'Zubin.Shah@antuit.com',
+    },
+    { firstName: 'Arjun',
+    lastName: 'Guntuka',
+    email: 'Arjun.Guntuka@antuit.com',
+    },
+    { firstName: 'Ram',
+    lastName: 'Singh',
+    email: 'Ram.Singh@antuit.com',
+    },
+    { firstName: 'Sujan',
+    lastName: 'Manandhar',
+    email: 'Sujan.Manandhar@antuit.com',
+    },
+
+
+]
+
+    return (
+       
                    
                 <Modal size={'3xl'}
                      initialFocusRef={initialRef}
@@ -28,6 +54,7 @@ const Share = (props: any) => {
                      <ModalHeader  color={shretextColor} mt={'13'} ml={20}>Share</ModalHeader>
                     <ModalCloseButton  mt={'12'} mr={8} color={'light.lighterGrayishBlue'} />
                     <Divider color={"default.dividerColor"}/>
+                   
                   <ModalBody pb={6} >
                     <FormControl>
                      <FormLabel  color={textColor2} mt={'20'} ml={20} mb={10}>Send to</FormLabel>
@@ -38,41 +65,47 @@ const Share = (props: any) => {
                     </Button>
                     </Flex>
                     </FormControl>
-
+                    
                     <FormControl mt={4}>
-
-                   <Box  borderColor={'light.lighterGrayishBlue'} borderWidth={1}  mt={20}  mb={20} ml={20}  mr={20}  width={'667px'} maxHeight={'472px'}>
+                    
+                   <Box  borderColor={'light.lighterGrayishBlue'} borderWidth={1}  mt={20}  mb={20} ml={20}  mr={20}  width={'667px'} maxHeight={'472px'}  >
                         <Text  mt={17} ml={16} color={accesstextColor}>Access by </Text>
+                        
                         <Flex as="nav" align="center" justify="space-between" wrap="wrap">
-                            <Center>
-                              <Avatar ml={16}  borderRadius="full" boxSize="32px" name={`${props.firstName} ${props.lastName}`} bg={'default.userCircleHeaderBg'} color={'default.userCircleHeaderFont'} />
-                              <Box mt={'17px'}>
-                                 <Text  ml={12} color={accesstextColor}>{props.firstName} {props.lastName}</Text>
-                                 <Text ml={12} pb={'10px'} color={'default.veryLightGrayTextColor'}>{props.email} </Text>
-                              </Box>
-                           </Center>
-                           <Center mr={'36px'} >  
-                              <Menu>
-                                <MenuButton>
-                                    <Text mr={'9px'} color={textColor2}> Can View</Text>
-                                </MenuButton>
-                                <MenuList width={121} borderRadius={'0'} ml={'-18px'} mt={'-2'} color={textColor}>
-                                    <MenuItem>Can View</MenuItem>
-                                    <MenuItem>Can Edits</MenuItem>
-                                    <Divider/>
-                                <MenuItem>Remove</MenuItem>
-                                </MenuList>
-                               </Menu>
-                            <DownArrowShare/>  
-                           </Center>
+
+                        {shareData.map((icons) => {
+                            return(
+                              
+                            <><Center>
+                                    <Avatar ml={16} borderRadius="full" boxSize="32px" name={`${icons.firstName} ${icons.lastName}`} bg={'default.userCircleHeaderBg'} color={'default.userCircleHeaderFont'} />
+                                    <Box mt={'17px'} width={'300px'}>
+                                        <Text ml={12} color={accesstextColor}>{icons?.firstName} {icons?.lastName}</Text>
+                                        <Text ml={12} pb={'10px'} color={'default.veryLightGrayTextColor'}>{icons.email} </Text>
+                                    </Box>
+                                </Center><Center mr={'36px'}>
+                                        <Menu>
+                                            <MenuButton>
+                                                <Text mr={'9px'} color={textColor2}> Can View</Text>
+                                            </MenuButton>
+                                            <MenuList width={121} borderRadius={'0'} ml={'-18px'} mt={'-2'} color={textColor}>
+                                                <MenuItem>Can View</MenuItem>
+                                                <MenuItem>Can Edits</MenuItem>
+                                                <Divider />
+                                                <MenuItem>Remove</MenuItem>
+                                            </MenuList>
+                                        </Menu>
+                                        <DownArrowShare />
+                                    </Center></>
+                       ) })}                           
                         </Flex>
+                      
                    </Box>
-                    </FormControl>
+                    
+                    </FormControl>    
                 </ModalBody>
-               
                 <Divider color={"default.dividerColor"}/>
-                
                   <ModalFooter >
+                    
                   
                  <Box width={'100%'} ml={20}>
                     <Flex>
---
+++ src/models/share.ts
@@ -0,0 +1,7 @@
+export interface ShareData {
+
+    firstName: string;
+    lastName: string;
+    email: string;
+
+}
