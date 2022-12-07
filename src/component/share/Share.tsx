a177f56ac17cccd84335500a85fecccf74127f82
Bampoori <SB6822@zebra.com>
2022-12-07 09:05:09 -0500

    add for changeing the branch

--- src/component/share/Share.tsx
+++ src/component/share/Share.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Box, Flex, Text, useColorModeValue, Editable, Button, Center, Avatar, Menu, MenuButton, MenuItem, MenuList, useDisclosure, ModalOverlay, ModalContent, ModalHeader, FormControl, ModalBody, Input, ModalCloseButton, ModalFooter, Modal, FormLabel, Divider, Stack, Link } from '@chakra-ui/react';
 import { ColorModeSwitcher } from '../colorModeSwitcher/ColorModeSwitcher';
 import { DownArrow, PencilIcon, LogoLight, GridCanvas, LineCanvasLogo, NoneCanvasLogo, DownArrowShare, LinkChain } from '../../assets/icons';
-import { config } from 'process';
 
-const Header = (props: any) => {
+
+
+
+const Share = (props: any) => {
     const themebg = useColorModeValue('light.header', 'dark.header');
     const textColor = useColorModeValue('light.header', 'default.whiteText');
     const textColor2 = useColorModeValue('default.blackText', 'default.whiteText');
@@ -16,31 +18,11 @@ const Header = (props: any) => {
     const finalRef = React.useRef(null)
     return (
         <Flex as="nav" align="center" justify="space-between" wrap="wrap" height={'44px'} pl={'4'} bg={themebg} color={'default.lightText'}>
-            <Box flex="3" ml={'2'}>
-                <LogoLight />
-            </Box>
-            <Center flex="3">
-                <Text color={'default.lightGrayHeader'} fontSize={'18'} fontStyle={'normal'}>
-                    Project Name
-                </Text>
-                <Text color={'default.lightGrayHeader'} pl={'2'}>
-                    /
-                </Text>
-                <Text color={'default.whiteText'} pl={'2'} fontSize={'18'} fontWeight={'700'} fontStyle={'normal'}>
-                    {' '}
-                    My New Experiment
-                </Text>
-
-                <Editable pl={'10'} defaultValue="Take some chakra">
-                    <PencilIcon />
-                </Editable>
-            </Center>
+            
 
             <Center flex="3" mr={5} justifyContent={'flex-end'} >
             <Box pl={'6'} pr={'6'} >
-                    <Button onClick={onOpen} colorScheme="default.lightGrayHeader" variant="outline">
-                        Share
-                    </Button>
+                   
                 <Modal size={'3xl'}
                      initialFocusRef={initialRef}
                      finalFocusRef={finalRef}
@@ -111,39 +93,10 @@ const Header = (props: any) => {
                  </ModalContent>
                   </Modal>
                 </Box>
-                <Box>
-                    {' '}
-                    <ColorModeSwitcher />
-                </Box>
-                <Menu>
-                    <MenuButton ml={'24'} >
-                    < GridCanvas/>
-                    </MenuButton>
-                    <MenuList borderRadius={'0'} width={'110'} color={textColor} ml={'-46'}>
-                        <MenuItem>< GridCanvas/><Text  ml={'12'}>Dot</Text></MenuItem>
-                        <MenuItem><LineCanvasLogo/><Text  ml={'12'}>Line</Text></MenuItem>
-                        <MenuItem><NoneCanvasLogo/><Text  ml={'12'}>None</Text></MenuItem>
-                    </MenuList>
-                </Menu>
-                <Box mr={'27'}  ml={'11'}>
-                <DownArrow />
-                </Box>
-                <Menu>
-                    <MenuButton>
-                        <Avatar borderRadius="full" boxSize="32px" name={`${props.firstName} ${props.lastName}`} bg={'default.userCircleHeaderBg'} color={'default.userCircleHeaderFont'} />
-                    </MenuButton>
-                    <MenuList width={127} borderRadius={'0'} mr={'-26'} mt={'-2'} color={textColor}>
-                        <MenuItem>My Profile</MenuItem>
-                        <MenuItem>Settings</MenuItem>
-                        <MenuItem>Signout</MenuItem>
-                    </MenuList>
-                </Menu>
-                <Box ml={'12'}  mr={'23'}>
-                   <DownArrow />
-                </Box>
+                
             </Center>
         </Flex>
     );
 };
 
-export default Header;
+export default Share;
