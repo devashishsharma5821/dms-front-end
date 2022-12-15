c46176eb16191a06d039916712852f3dfc8c4f3f
zshah-antuit <zubin.shah@zebra.com>
2022-12-15 13:30:04 -0500

    Formic

--- src/component/settings/Settings.tsx
+++ src/component/settings/Settings.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Button, Divider,useDisclosure, Modal, ModalContent, ModalHeader, ModalOverlay, ModalCloseButton, ModalBody, FormControl, Input, FormLabel, ModalFooter } from '@chakra-ui/react';
+import { Formik, Field } from "formik";
+import { Button, Divider, useDisclosure, VStack, Modal, ModalContent, ModalHeader, ModalOverlay, ModalCloseButton, ModalBody, FormControl, Input, FormLabel, ModalFooter, FormErrorMessage } from '@chakra-ui/react';
 
 const Settings = (props: any) => {
     const {  onClose } = useDisclosure();
-    const initialRef = React.useRef(null)
-    const finalRef = React.useRef(null)
+    const initialRef = React.useRef(null);
+    const finalRef = React.useRef(null);
+    interface databricksSettings {
+    userName: string;
+    token: string;
+  }
     return (
         <Modal
         initialFocusRef={initialRef}
@@ -18,15 +23,63 @@ const Settings = (props: any) => {
           <ModalCloseButton />
           <Divider color={"default.dividerColor"} mt={'20px'} mb={'20px'}/>
           <ModalBody pb={6}  mr={'10px'} ml={'10px'}>
-            <FormControl>
-              <FormLabel as='b'>Username</FormLabel>
-              <Input ref={initialRef} placeholder='User Name' />
-            </FormControl>
+            <Formik
+                initialValues={{
+                  userName: "",
+                  token: ""
+                } as databricksSettings}
+                onSubmit={(values) => {
+                 console.log('Values', values)
+                }}
+            >
+              {({ handleSubmit, errors, touched }) => (
+                  <form onSubmit={handleSubmit}>
+                    <VStack spacing={4} align="flex-start">
+                      <FormControl>
+                        <FormLabel htmlFor="userName">User Name</FormLabel>
+                        <Field
+                            as={Input}
+                            id="userName"
+                            name="userName"
+                            type="userName"
+                            variant="filled"
+                        />
+                      </FormControl>
+                      <FormControl isInvalid={!!errors.token && touched.token}>
+                        <FormLabel htmlFor="password">Password</FormLabel>
+                        <Field
+                            as={Input}
+                            id="token"
+                            name="token"
+                            type="token"
+                            variant="filled"
+                            validate={(value: any) => {
+                              let error;
+                              if (value.length < 5) {
+                                error = "Token must contain at least 6 characters";
+                              }
 
-            <FormControl mt={4}>
-              <FormLabel as='b'>Token</FormLabel>
-              <Input placeholder='Token' />
-            </FormControl>
+                              return error;
+                            }}
+                        />
+                        <FormErrorMessage>{errors.token}</FormErrorMessage>
+                      </FormControl>
+                      <Button type="submit" colorScheme="purple" width="full">
+                        Login
+                      </Button>
+                    </VStack>
+                  </form>
+              )}
+            </Formik>
+            {/*<FormControl>*/}
+            {/*  <FormLabel as='b'>Username</FormLabel>*/}
+            {/*  <Input ref={initialRef} placeholder='User Name' />*/}
+            {/*</FormControl>*/}
+
+            {/*<FormControl mt={4}>*/}
+            {/*  <FormLabel as='b'>Token</FormLabel>*/}
+            {/*  <Input placeholder='Token' />*/}
+            {/*</FormControl>*/}
           </ModalBody>
           <Divider color={"default.dividerColor"} mt={'20px'} mb={'20px'}/>
           <ModalFooter mb={'20px'}>
