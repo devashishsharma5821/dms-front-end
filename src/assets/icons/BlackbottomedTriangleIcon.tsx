a0b03a00cce327c79aabe479da9ee0626cdb81ce
ZGN\AG7955 <arjun.guntuka@zebra.com>
2022-12-15 13:18:03 -0600

    create compute modal UI changes added

---
+++ src/assets/icons/BlackbottomedTriangleIcon.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+type LogoProps = {
+    children?: never;
+};
+
+export const BlackbottomedTriangleIcon: React.FC<LogoProps> = () => {
+    return (
+       <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.942005 0.0292969H13.058C13.8962 0.0292969 14.3153 1.04171 13.7219 1.63503L7.66631 7.69538C7.29901 8.06267 6.70098 8.06267 6.33369 7.69538L0.278051 1.63503C-0.31527 1.04171 0.103822 0.0292969 0.942005 0.0292969Z" fill="#666C80"/>
+</svg>
+
+    );
+};
+
+export default BlackbottomedTriangleIcon;
--- src/component/sideBarMenu/SideBarMenu.tsx
+++ src/component/sideBarMenu/SideBarMenu.tsx
@@ -1,9 +1,11 @@
 import React, { useMemo } from 'react';
-import { Box, Flex, Divider, useColorModeValue, Center, VStack, Square } from '@chakra-ui/react';
+import { Box, Flex, Divider, useColorModeValue, Center, VStack, Square,  Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, Button, FormControl, FormLabel, Input, Select, Switch, Text, Tooltip } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import sideBarMenuIcons from '../../models/sideBarMenuData';
 import './style.scss';
-import { CreateIcon, WideCreateIcon } from '../../assets/icons';
+import { CreateIcon, WideCreateIcon} from '../../assets/icons';
+import BlackbottomedTriangleIcon from '../../assets/icons/BlackbottomedTriangleIcon';
+import InfoIcon from '../../assets/icons/InfoIcon';
 
 const SideBarMenu = () => {
     const themebg = useColorModeValue('light.header', 'dark.header');
@@ -11,6 +13,7 @@ const SideBarMenu = () => {
     const [activateSubMenu, setActivateSubMenu] = React.useState(false);
     const zIndexStyle = useMemo(() => ({ zIndex: '10000' }), []);
      const [currentIndex, setCurrentIndex] = React.useState(0);
+     const { isOpen, onOpen, onClose } = useDisclosure();
     const navigate = useNavigate();
     const hoverIn = () => {
         setIsHovering(true);
@@ -44,6 +47,7 @@ const SideBarMenu = () => {
             setActivateSubMenu(true);
         }
     };
+    
    const secondLevelMenu = () => {
        console.log('Switch', sideBarMenuIcons);
      return (
@@ -109,8 +113,8 @@ const SideBarMenu = () => {
                         )} 
                         {isHovering && (
                             <Box width="212px"  mt="30">
-                                  <Box  width={'200px'} ml={'6px'} mr={'6px'} height={'40px'} mb={'9px'} >
-                                    <Square ><WideCreateIcon/></Square>
+                                  <Box  width={'200px'} ml={'6px'} mr={'6px'} height={'40px'} mb={'9px'} onClick={onOpen}>
+                                    <Square ><WideCreateIcon /></Square>
                                  </Box>
                                 {sideBarMenuIcons.map((sections, sectionIndex) => {
                                     const lastItemLength = sections.section.length - 1;
@@ -151,6 +155,70 @@ const SideBarMenu = () => {
                         )}
                     </VStack>
                 </Flex>
+                
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent width={630} maxWidth={630} height="var(--chakra-space-464)" color="#171717">
+          <ModalHeader height="var(--chakra-space-60)" fontSize={16} borderBottom="1px solid #EAEAEA" fontWeight="700" flex={'none'} padding={20}>Edit Compute</ModalHeader>
+          <ModalCloseButton mt={10}/>
+          <ModalBody padding={20}>
+           <FormControl>
+                <FormLabel>Compute Name <Text as='sup' color='#ED1C24' fontSize={16} fontWeight="bold" verticalAlign={'sub'}>*</Text></FormLabel>
+                <Input type='text' />
+                
+            </FormControl>
+            <Box mt={22} display="flex" mb={20}>
+            <FormControl width={327}>
+                <FormLabel>Worker Type <Text as='sup' color='#ED1C24' fontSize={16} fontWeight="bold" verticalAlign={'sub'}>*</Text><Tooltip label='Right end' placement='right-end' hasArrow ><Text display={'inline-block'} ml={2}><InfoIcon /></Text></Tooltip></FormLabel>
+                <Select placeholder='Standard_DS3_v2   56 GB | 16 Cores' icon={<Text mt={10} marginRight={'13'}><BlackbottomedTriangleIcon/></Text> }>
+                    <option>Standard_DS3_v2   56 GB | 16 Cores </option>
+                </Select>
+            </FormControl>
+            <FormControl width={112} ml={24} >
+                <FormLabel>Min Workers<Text as='sup' color='#ED1C24' fontSize={16} fontWeight="bold" verticalAlign={'sub'}>*</Text></FormLabel>
+                <Input type='text' />
+                
+            </FormControl>
+            <FormControl width={113} ml={16}>
+                <FormLabel>Max Workers<Text as='sup' color='#ED1C24' fontSize={16} fontWeight="bold" verticalAlign={'sub'}>*</Text></FormLabel>
+                <Input type='text' />
+                
+            </FormControl>
+            </Box>
+            <FormControl display='flex' alignItems='center' mb={16}>
+                <Switch id='spot-instances' width={30} height={15}/>
+                <FormLabel htmlFor='spot-instances' mb='0' ml={7}>
+                    Spot instances
+                </FormLabel>
+            </FormControl>
+            <FormControl display='flex' alignItems='center' mb={14}>
+                <Switch id='enable-autoscaling' />
+                <FormLabel htmlFor='enable-autoscaling' mb='0' ml={7}>
+                    Enable autoscaling
+                </FormLabel>
+                
+            </FormControl>
+            <FormControl display='flex' alignItems='center'>
+                <Switch id='terminate-after' />
+                <FormLabel htmlFor='terminate-after' mb='0' ml={7}>
+                    Terminate after
+                </FormLabel>
+                <Input type='text' width='var(--chakra-space-44)'/>
+                <FormLabel mb='0' ml={8}>
+                    minutes of inactivity
+                </FormLabel>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter height="var(--chakra-space-75)" borderTop="1px solid #EAEAEA" padding={'20'}>
+            <Button colorScheme='blue' fontSize={16}  variant='outline' pt={'10'} pb={'10'} pl={'17'} pr={'17'}>
+                Cancel
+            </Button>
+            <Button colorScheme='blue' fontSize={16}  ml={15} pt={'10'} pb={'10'} pl={'17'} pr={'17'}>Create</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
             </div>
             {
                 activateSubMenu && secondLevelMenu()
