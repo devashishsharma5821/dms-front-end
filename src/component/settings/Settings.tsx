4d9a66b9d5480881a302ef70a463a26b461c6d85
zshah-antuit <zubin.shah@zebra.com>
2022-12-19 12:54:10 -0500

    Final Databricks Push

--- src/component/settings/Settings.tsx
+++ src/component/settings/Settings.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Formik, Field } from "formik";
-import { Button, Divider, VStack, Modal, ModalContent, ModalHeader, ModalOverlay, ModalCloseButton, ModalBody, FormControl, Input, FormLabel, ModalFooter, FormErrorMessage, Spinner } from '@chakra-ui/react';
+import { Button, Divider, VStack, Modal, ModalContent, ModalHeader, ModalOverlay, ModalCloseButton, ModalBody, FormControl, Input, FormLabel, ModalFooter, FormErrorMessage } from '@chakra-ui/react';
 import {setSettingsData} from "../../query";
 import { useApolloClient } from "@apollo/client";
 const Settings = (props: any) => {
@@ -36,18 +36,17 @@ const Settings = (props: any) => {
                 validateOnBlur={true}
                 validateOnChange={true}
                 onSubmit={(values) => {
-                 console.log('Values', values)
+                    setLoading(true);
                  client.mutate({
                     mutation: setSettingsData(values.userName ,values.token)
                 })
                     .then((response) => {
-                    console.log('Response is:',response)
                     if(response.data.dmsSetDatabricksCredentials){
-                        props.onClose()
+                        props.onClose();
+                        setLoading(false);
                     }
                     })
-                    .catch((err) => console.log(err));
-                    
+                    .catch((err) => console.error(err));
                 }}
             >
               {({ handleSubmit, errors, touched ,isValid}) => (
@@ -63,9 +62,8 @@ const Settings = (props: any) => {
                             validate={(value: any) => {
                                 let error;
                                 if (value.length === 0) {
-                                  error = "User Name is requierd";
+                                  error = "User Name is required";
                                 }
-  
                                 return error;
                               }}
                            
@@ -82,7 +80,7 @@ const Settings = (props: any) => {
                             validate={(value: any) => {
                               let error;
                               if (value.length === 0) {
-                                error = "Token is requierd";
+                                error = "Token is required";
                               }
 
                               return error;
@@ -92,19 +90,18 @@ const Settings = (props: any) => {
                         <Divider color={"default.dividerColor"} mt={'20px'} mb={'20px'} width="full"/>
 
                       </FormControl>
-                      <ModalFooter mb={'20px'} > 
-                      
-                      <Button disabled={!isValid || (Object.keys(touched).length === 0 && touched.constructor === Object) } onSubmit={props.onSubmit} type="submit" colorScheme="blue" width="65px" >
-                       Ok
-                       {loading && <Spinner/>   }
-                      </Button>
-                     
-                    <Button onClick={props.onClose} colorScheme='blue' ml={'5px'}>Cancel</Button>
+                      <ModalFooter mb={'20px'} >
+                          {
+                              (loading) ?
+                                  <Button isLoading disabled={!isValid || (Object.keys(touched).length === 0 && touched.constructor === Object) } onSubmit={props.onSubmit} type="submit" colorScheme="blue" width="65px" >Ok</Button>
+                                  : <Button  disabled={!isValid || (Object.keys(touched).length === 0 && touched.constructor === Object) } onSubmit={props.onSubmit} type="submit" colorScheme="blue" width="65px" >Ok</Button>
+                          }
+                          <Button disabled={loading} onClick={props.onClose} colorScheme='blue' ml={'5px'}>Cancel</Button>
                     </ModalFooter>
                     </VStack>
                   </form>
               )}
-            </Formik>   
+            </Formik>
           </ModalBody>
        </ModalContent>
       </Modal>
