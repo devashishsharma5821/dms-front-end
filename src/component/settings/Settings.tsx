52cd62e9ba6210b786b10e2b11a3abbacaf42b56
Bampoori <SB6822@zebra.com>
2022-12-19 12:23:42 -0500

    add spinner

--- src/component/settings/Settings.tsx
+++ src/component/settings/Settings.tsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
 import { Formik, Field } from "formik";
-import { Button, Divider, VStack, Modal, ModalContent, ModalHeader, ModalOverlay, ModalCloseButton, ModalBody, FormControl, Input, FormLabel, ModalFooter, FormErrorMessage } from '@chakra-ui/react';
+import { Button, Divider, VStack, Modal, ModalContent, ModalHeader, ModalOverlay, ModalCloseButton, ModalBody, FormControl, Input, FormLabel, ModalFooter, FormErrorMessage, Spinner } from '@chakra-ui/react';
 import {setSettingsData} from "../../query";
 import { useApolloClient } from "@apollo/client";
 const Settings = (props: any) => {
     const initialRef = React.useRef(null);
     const finalRef = React.useRef(null);
+    const [loading, setLoading] = useState(false);
     const client = useApolloClient();
     interface databricksSettings {
     userName: string;
     token: string;
      }
+
     return (
         <Modal
         initialFocusRef={initialRef}
         finalFocusRef={finalRef}
         isOpen={props.isOpen}
         onClose={props.onClose}
+        isCentered
       >
         <ModalOverlay />
         <ModalContent>
@@ -26,9 +29,12 @@ const Settings = (props: any) => {
           <ModalBody pb={6}  mr={'10px'} ml={'10px'}>
             <Formik
                 initialValues={{
+                  validateOnMount: true,
                   userName: "",
                   token: ""
                 } as databricksSettings}
+                validateOnBlur={true}
+                validateOnChange={true}
                 onSubmit={(values) => {
                  console.log('Values', values)
                  client.mutate({
@@ -44,17 +50,27 @@ const Settings = (props: any) => {
                     
                 }}
             >
-              {({ handleSubmit, errors, touched }) => (
+              {({ handleSubmit, errors, touched ,isValid}) => (
                   <form onSubmit={handleSubmit}>
                     <VStack spacing={4} align="flex-start">
-                      <FormControl>
+                      <FormControl  isInvalid={!!errors.userName && touched.userName}>
                         <FormLabel htmlFor="userName">User Name</FormLabel>
                         <Field
                             as={Input}
                             id="userName"
                             name="userName"    
                             variant="filled"
+                            validate={(value: any) => {
+                                let error;
+                                if (value.length === 0) {
+                                  error = "User Name is requierd";
+                                }
+  
+                                return error;
+                              }}
+                           
                         />
+                             <FormErrorMessage>{errors.userName}</FormErrorMessage>
                       </FormControl>
                       <FormControl isInvalid={!!errors.token && touched.token}>
                         <FormLabel htmlFor="token">Token</FormLabel>
@@ -65,31 +81,32 @@ const Settings = (props: any) => {
                             variant="filled"
                             validate={(value: any) => {
                               let error;
-                              if (value.length < 5) {
-                                error = "Token must contain at least 6 characters";
+                              if (value.length === 0) {
+                                error = "Token is requierd";
                               }
 
                               return error;
                             }}
                         />
                         <FormErrorMessage>{errors.token}</FormErrorMessage>
+                        <Divider color={"default.dividerColor"} mt={'20px'} mb={'20px'} width="full"/>
+
                       </FormControl>
-                      <Button type="submit" colorScheme="blue" width="65px" >
+                      <ModalFooter mb={'20px'} > 
+                      
+                      <Button disabled={!isValid || (Object.keys(touched).length === 0 && touched.constructor === Object) } onSubmit={props.onSubmit} type="submit" colorScheme="blue" width="65px" >
                        Ok
+                       {loading && <Spinner/>   }
                       </Button>
+                     
+                    <Button onClick={props.onClose} colorScheme='blue' ml={'5px'}>Cancel</Button>
+                    </ModalFooter>
                     </VStack>
                   </form>
               )}
-            </Formik>
-        
+            </Formik>   
           </ModalBody>
-          <Divider color={"default.dividerColor"} mt={'20px'} mb={'20px'} width="full"/>
-                      <ModalFooter mb={'20px'} >
-                     
-                      
-                    <Button onClick={props.onClose} colorScheme='blue' ml={'5px'}>Cancel</Button>
-                    </ModalFooter>
-        </ModalContent>
+       </ModalContent>
       </Modal>
     );
 };
