2706782c25f0d141fc79b5ec240c3ea3a1d4f8f4
ram-antuit <ram.singh@zebra.com>
2023-04-10 12:44:48 +0530

    Removed hardcoding espuser token

--- src/apollo-client.tsx
+++ src/apollo-client.tsx
@@ -21,9 +21,8 @@ if (location.href.indexOf('localhost') > 0) {
 const authLink = setContext((_, { headers }) => {
     // get the authentication token from local storage if it exists
     const token = localStorage.getItem('accessToken');
-    // const espUserToken = localStorage.getItem('espUserToken');
-    const espUserToken =
-        'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mbyI6eyJpZCI6ImVlYTc1Y2Y5LTA2ZGUtNGZlNy1iNDc2LWFjNDg4NTYzOThkZiIsIm5hbWUiOiJadWJpbiBTaGFoIiwiZ2l2ZW5OYW1lIjoiWnViaW4iLCJmYW1pbHlOYW1lIjoiU2hhaCIsImVtYWlsIjoienViaW4uc2hhaEBhbnR1aXQuY29tIiwidXNlckV4dGVybmFsSWQiOiI1ZjM4ZmIzMy1kMjhiLTRiNTctYTRkMy0zMjA3NWI5NDNhMzgifSwiaWF0IjoxNjc1MjI1MjEyLCJleHAiOjE2ODU1OTMyMTIsImF1ZCI6Imh0dHBzOi8vd3d3LmFudHVpdC5haS8iLCJpc3MiOiJBbnR1aXQgQUkiLCJzdWIiOiJhZG1pbkBhbnR1aXQuYWkifQ.EGX6MTLl9CVmt34Q6YdXsiE2Hg6k_F3_RLv3r4tKpdfPd3WQbiUjaVzWeeFFiEx_TjsQk3LZpYojaAxnl2fJWg';
+    const espUserToken = localStorage.getItem('espUserToken');
+    
     console.log('headers', headers);
     // return the headers to the context so httpLink can read them
     return {
